e48e3080d1ba659a16b559d780d08ed2b049e336
Tran Nguyen <tranhnguyen@cpp.edu>
2022-03-08 18:53:51 -0800

    Working Project

--- lo-shu-square.c
+++ lo-shu-square.c
@@ -28,21 +28,36 @@ int main(){
     srand((unsigned) time(&t));
     int totalSquares = 0;
 
-    
+  
     do {
-        for(int row = 0; row < SIZE;row++){
-    
-            for(int col = 0; col < SIZE; col++ ){
-                number = rand () % 9 + 1;
+        for (int i = 0; i<9;i++){
+            checkArr[i]=0;
 
+        }
+        
+        for(int row = 0; row < SIZE;row++){
+            for(int col = 0; col < SIZE; col++ ){
+                    number = rand () % 9 + 1;    
+                if(checkArr[number-1] == 0){
+                    checkArr[number-1] = 1;
+                    array[row][col] = number;
+                }
+                else{
+                    col--;
+                }
+                
+            
              } 
        
-        }    
-
+        }
+        //printSquare(array);
+        totalSquares++; 
     }
     while (!loShu(array));
 
+    printf("The total number of squares generated: %d\n",totalSquares);
 
+    printSquare(array);  
 
 
 
@@ -84,8 +99,9 @@ bool loShu(int array[][SIZE]){
             if (row == 1){
                 diagSum1 += array[col][col];
                 diagSum2 += array[SIZE -1 - col][col];
-                colSum = diagSum1;
-                rowSum = diagSum2;
+                colSum += array[col][row];
+                rowSum += array[row][col];
+               
 
             }
             else{
