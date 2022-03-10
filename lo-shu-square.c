4db35513b6c500d86261e6e977bb90bcc4351302
Tran Nguyen <tranhnguyen@cpp.edu>
2022-03-10 01:35:08 -0800

    Working version

--- lo-shu-square.c
+++ lo-shu-square.c
@@ -3,38 +3,43 @@
 #include <time.h>
 #include <stdbool.h>
 
-const int SIZE = 3;
+const int SIZE = 3;//Define the side of the square
 
+//Function declarion
 bool loShu(int array[][SIZE]);
 void printSquare(int array[][SIZE]);
 
 
-
 int main(){
+    int array_1[3][3] = { 4,9,2,
+                        3,5,7,
+                        8,1,6 };  
 
+    int array_2[3][3] = { 1,2,3,
+                        4,5,6,
+                        7,8,9};  
 
-    int array1[3][3] = { 4,9,2,
-                               3,5,7,
-                               8,1,6 };  
-
-    int array2[3][3] = { 1,2,3,
-                               4,5,6,
-                               7,8,9};  
-
-    int checkArr[9] ={0};
+    int checkArr[9] ={0}; //Array to check if 1->9 has been used or not
     int array[3][3];
-    int number = 0;
+    int number; //Random number
     time_t t;
     srand((unsigned) time(&t));
     int totalSquares = 0;
+
+
+     
+    
+    printSquare(array_1);
+    printf("%s\n\n", loShu(array_1) ? "This is a Lo Shu Square!": "This is NOT a Lo Shu Square!");
+   
+    printSquare(array_2);
+    printf("%s\n\n", loShu(array_2) ? "This is a Lo Shu Square!": "This is NOT a Lo Shu Square!");
 
   
     do {
         for (int i = 0; i<9;i++){
             checkArr[i]=0;
-
         }
-        
         for(int row = 0; row < SIZE;row++){
             for(int col = 0; col < SIZE; col++ ){
                     number = rand () % 9 + 1;    
@@ -45,25 +50,18 @@ int main(){
                 else{
                     col--;
                 }
-                
-            
+
              } 
        
         }
-        //printSquare(array);
         totalSquares++; 
     }
     while (!loShu(array));
 
     printf("The total number of squares generated: %d\n",totalSquares);
-
     printSquare(array);  
 
-
-
-    bool ans = loShu(array2);
-    printf("%s\n", ans ? "True": "False");
-    printSquare(array1);
+ 
 
 
     return 0;
@@ -92,27 +90,26 @@ bool loShu(int array[][SIZE]){
         diagSum2 = 0;
         for(int col = 0; col < SIZE; col++ ){
             if (row == 0){
-                target += array[row][col];     
+                target += array[row][col];//Set target = the first row
             }
-            colSum += array[col][row];
-            rowSum += array[row][col];
-            if (row == 1){
+
+            colSum += array[col][row];//Calculate col at index
+            rowSum += array[row][col];//Calculate row at intex
+
+            if (row == 1){//Only calculate the diagional when row = 1          
                 diagSum1 += array[col][col];
                 diagSum2 += array[SIZE -1 - col][col];
-
             }
-            else{    //Assumtion when we dont check diagonals
+            else{//Assumtion when we dont calculate diagonals when row = 2
                 diagSum1 = colSum;
                 diagSum2 = rowSum;
             }   
         }
-
-        //printf("Target:%d  row:%d  col:%d  d1:%d  d2:%d \n", target, rowSum, colSum, diagSum1,diagSum2);
-        if (target != rowSum || target != colSum || target != diagSum1 || target != diagSum2){
+        //Check after each row
+        if (target != rowSum || target != colSum || target != diagSum1 || target != diagSum2){ 
             return false;
         }   
     }
 
     return true;
-
 }
